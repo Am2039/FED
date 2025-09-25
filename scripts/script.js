90f79caadfb8e6baca6e453433b89ca6f96bb740
Am2039 <amber.baart@hva.nl>
2025-09-25 10:37:48 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,41 @@
-// JavaScript Document
-console.log("hi");
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
+
+
+
+
+
+
+
+document.querySelectorAll(".carousel").forEach(carousel => {
+    const items = carousel.querySelectorAll(".carousel__item");
+    const buttonHtml = Array.from(items, () => {
+        return `<span class="carousel__button"></span>`;
+    });
+
+    carousel.insertAdjacentHTML("beforeend", `
+        <div class="carousel__nav">
+            ${ buttonHtml.join("") }
+        </div>
+     `);
+
+    const buttons = carousel.querySelectorAll(".carousel__button");
+
+    buttons[0].classList.add("carousel__button--selected");
+
+    buttons.forEach((button, i) => {
+        button.addEventListener("click", () => {
+            items.forEach(item => item.classList.remove("carousel__item--selected"));
+            buttons.forEach(button => button.classList.remove("carousel__button--selected"));
+
+            items[i].classList.add("carousel__item--selected");
+            button.classList.add("carousel__button--selected");
+        });
+    });
+});
