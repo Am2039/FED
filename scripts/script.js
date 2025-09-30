001a5276e4a8c59e1b589e34291d8e72ec3b51c8
Am2039 <amber.baart@hva.nl>
2025-09-30 13:59:18 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
-const hamMenu = document.querySelector('.ham-menu');
 
-const offScreenMenu = document.querySelector('.off-screen-menu');
+/* Bron: https://www.youtube.com/watch?v=aNDqzlAKmZc */
+const hamMenu = document.querySelector('.ham-menu'); /* Verwijst naar html */
 
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
+const offScreenMenu = document.querySelector('.off-screen-menu'); /* Menu als klik op hamburger menu */
+
+hamMenu.addEventListener('click', () => { 
+    hamMenu.classList.toggle('active'); /* Veranderd hamburger knop in kruisje */
     offScreenMenu.classList.toggle('active');
 })
 
@@ -12,14 +14,15 @@ hamMenu.addEventListener('click', () => {
 
 
 
-
-document.querySelectorAll(".carousel").forEach(carousel => {
-    const items = carousel.querySelectorAll(".carousel__item");
-    const buttonHtml = Array.from(items, () => {
+/* Bron: https://www.youtube.com/watch?v=XtFlpgaLbZ4&t=66s */
+document.querySelectorAll(".carousel").forEach(carousel => { /* Verwijst naar carousel in html */
+    const items = carousel.querySelectorAll(".carousel__item"); /* Verwijst naar inhoud carousel (tekst) */
+    const buttonHtml = Array.from(items, () => { /* Hierdoor komen de 4 dots */
         return `<span class="carousel__button"></span>`;
     });
 
-    carousel.insertAdjacentHTML("beforeend", `
+    /* Knoppen in de carousel */
+    carousel.insertAdjacentHTML("beforeend", ` 
         <div class="carousel__nav">
             ${ buttonHtml.join("") }
         </div>
@@ -27,9 +30,10 @@ document.querySelectorAll(".carousel").forEach(carousel => {
 
     const buttons = carousel.querySelectorAll(".carousel__button");
 
-    buttons[0].classList.add("carousel__button--selected");
+    buttons[0].classList.add("carousel__button--selected"); /* Maakt de eerste slide actief */
 
-    buttons.forEach((button, i) => {
+    /* Maakt dots klikbaar */
+    buttons.forEach((button, i) => { 
         button.addEventListener("click", () => {
             items.forEach(item => item.classList.remove("carousel__item--selected"));
             buttons.forEach(button => button.classList.remove("carousel__button--selected"));
